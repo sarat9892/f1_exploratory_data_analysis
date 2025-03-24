d87c8a2583a60cc0883135169fd5082b40b71fb3
sarat <sarat9892@gmail.com>
2025-03-24 23:56:12 +1100

    Adding a tooltip to the chart

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import pandas as pd
 from bokeh.plotting import figure, show, save
+from bokeh.models import ColumnDataSource, HoverTool
+from collections import defaultdict
+
 
 def dominant_driver_chart(data_frame):
-    years = data_frame["Season"]
-    win_count = data_frame["No_of_Wins"]
-    race_count = data_frame["No_of_Races"]
-    world_champion = data_frame["World_Champion"]
-    max_winning_drivers = data_frame["MaxWin_Drivers"]
+
+    source_list = defaultdict(list)
+
+    source_list["years"] = list(data_frame["Season"])
+    source_list["win_count"] = list(data_frame["No_of_Wins"])
+    source_list["race_count"] = list(data_frame["No_of_Races"])
+    source_list["world_champion"] = list(data_frame["World_Champion"])
+    source_list["max_winning_drivers"] = list(data_frame["MaxWin_Drivers"])
+
+    source = ColumnDataSource(source_list)
 
     plot = figure(title="Dominant Drivers", x_axis_label="Season", y_axis_label="Races", frame_width=1000)
 
-    plot.vbar(x=years, top=race_count, legend_label="Number of Races", width=0.8, bottom=0, color="blue")
-    plot.vbar(x=years, top=win_count, legend_label="Number of Wins", width=0.8, bottom=0, color="red")
+    plot.vbar(x="years", top="race_count", legend_label="Number of Races", width=0.8, bottom=0, color="blue", source=source)
+    plot.vbar(x="years", top="win_count", legend_label="Number of Wins", width=0.8, bottom=0, color="red", source=source)
+
+    plot.add_tools(HoverTool(tooltips=[("World Champion", "@world_champion"),
+                                       ("Driver With Most Wins", "@max_winning_drivers")]))
+
+    plot.legend.location = "top_left"
 
     save(plot, "f1_dominant_drivers.html", resources="cdn", title="F1 Dominant Drivers")
     show(plot)
